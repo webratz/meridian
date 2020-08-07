57af087f4d110c8b71b1ec404795051e8795220b
Andreas Sieferlinger <andi@maggie.webratz.de>
2020-08-07 13:47:07 +0000

    minor tweaks

--- meridian_notifier.py
+++ meridian_notifier.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf8 -*-
 #
 # This script scrapes the HTML of an website to generate notifications if something new is there
@@ -8,7 +8,6 @@
 # Copyright (c) 2016 Andreas Sieferlinger
 
 import requests
-from datetime import datetime
 import sys
 import os
 import logging
@@ -41,7 +40,10 @@ ch.setLevel(logging.WARNING)
 logger.addHandler(ch)
 
 # mail errors to my owner
-mailh = handlers.SMTPHandler(config['maillogger']['mailhost'], config['maillogger']['fromaddr'], config['maillogger']['toaddr'], 'Meridian Maillogger Error')
+mailh = handlers.SMTPHandler(config['maillogger']['mailhost'],
+                             config['maillogger']['fromaddr'],
+                             config['maillogger']['toaddr'],
+                             'Meridian Maillogger Error')
 mailh.setLevel(logging.ERROR)
 logger.addHandler(mailh)
 
@@ -58,7 +60,6 @@ class MeridianInterruption(object):
         self.category = category
         self.id = self.compute_id()
         self.mid = mid  # Meridian ID
-
 
     def compute_id(self):
         """
@@ -82,30 +83,33 @@ Kategorie: %s
 Links:
         """ % (self.start, self.end, self.content, self.category)
 
-
         for channel in pb.channels:
             if config['pushbullet']['channel'] in channel.name:
                 channel.push_note(self.headline, text)
         return text
 
     def twitter(self):
-        twapi = twitter.Api(consumer_key=config['twitter']['consumer_key'],
-                  consumer_secret=config['twitter']['consumer_secret'],
-                  access_token_key=config['twitter']['access_token_key'],
-                  access_token_secret=config['twitter']['access_token_secret'],
-                  input_encoding='utf8')
+        twapi = twitter.Api(
+            consumer_key=config['twitter']['consumer_key'],
+            consumer_secret=config['twitter']['consumer_secret'],
+            access_token_key=config['twitter']['access_token_key'],
+            access_token_secret=config['twitter']['access_token_secret'],
+            input_encoding='utf8')
 
-        message = "%s: %s - %s" % (self.category, self.headline, MERIDIAN_INTERRUPTION_URL)
+        message = "%s: %s - %s" % (self.category, self.headline,
+                                   MERIDIAN_INTERRUPTION_URL)
         try:
             logger.info("sent twitter message for id %s", self.id)
             twapi.PostUpdates(status=message)
         except Exception as e:
-            logger.error("Could not post twitter message for id %s %s", self.id, e)
+            logger.error("Could not post twitter message for id %s %s",
+                         self.id, e)
 
 
 # scrape the hell out of the website
 class MeridianInterruptionPage(object):
-    def __init__(self, url='http://www.meridian-bob-brb.de/de/baustellen.json'):
+    def __init__(self,
+                 url='http://www.meridian-bob-brb.de/de/baustellen.json'):
         self.url = url
         self.interruptions = []
         self.data = self.get_data()
@@ -117,8 +121,10 @@ class MeridianInterruptionPage(object):
         """
         try:
             r = requests.get(self.url)
-            if r.status_code == requests.codes.ok: # pylint: disable=E1101
+            if r.status_code == requests.codes.ok:  # pylint: disable=E1101
                 return r.json()
+            else:
+                raise requests.exceptions.HTTPError()
 
         except requests.exceptions.RequestException as e:
             logger.error('Could not load the the json')
@@ -126,7 +132,7 @@ class MeridianInterruptionPage(object):
             sys.exit(1)
 
     def parse_data(self):
-        for element in self.data.get('current'): #  TODO: add also upcoming
+        for element in self.data.get('current'):  # TODO: add also upcoming
             headline = element['title']
             content = element['body']
             category = element['category']
@@ -134,28 +140,34 @@ class MeridianInterruptionPage(object):
             try:
                 end = iso8601.parse_date(element['ends_at'])
             except iso8601.iso8601.ParseError:
-                end = None            
+                end = None
             mid = element['id']
             if 1 in element['line_ids']:
                 # print element['id'], headline
-                self.interruptions.append(MeridianInterruption(headline, content, start, end, category, mid))
-
-
+                self.interruptions.append(
+                    MeridianInterruption(headline, content, start, end,
+                                         category, mid))
 
 
 def run():
     """
     actually glue everything together and run
     """
-    mip = MeridianInterruptionPage()
+    try:
+        mip = MeridianInterruptionPage()
+    except requests.exceptions.HTTPError:
+        print('could not fetch data')
+        sys.exit(1)
 
     # load a file to check what we already have notified
     try:
-        already_notified = pickle.load(open(CONFDIR + "/notify_store.bin", "rb"))
+        already_notified = pickle.load(
+            open(CONFDIR + "/notify_store.bin", "rb"))
     except IOError:
-        logger.warning('Could not load already_notified list. Starting with an empty list')
+        logger.warning(
+            'Could not load already_notified list. Starting with an empty list'
+        )
         already_notified = []
-
 
     for i in mip.interruptions:
         if i.id not in already_notified:
@@ -166,12 +178,13 @@ def run():
             already_notified.append(i.id)
         logger.info('NOT sending a notification for id %s', i.id)
 
-
     # store the list of notified itmes back to disk
     try:
-        pickle.dump(already_notified, open(CONFDIR + "/notify_store.bin", "wb"))
+        pickle.dump(already_notified, open(CONFDIR + "/notify_store.bin",
+                                           "wb"))
     except IOError:
         logger.warning('Could not save already_notified list')
+
 
 run()
 
